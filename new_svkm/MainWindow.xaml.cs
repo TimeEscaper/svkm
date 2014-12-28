82eeeb03014867118a85788015512adb22743067
TimeEscaper <rentgeny05@gmail.com>
2014-12-28 15:08:03 +0400

    svkm
    sdf

---
+++ new_svkm/MainWindow.xaml.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.Serialization.Formatters;
+using System.Text.RegularExpressions;
+
+namespace new_svkm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int appid = 4015559;
+
+        int userid;
+        string token;
+
+        string cache_path = Environment.CurrentDirectory + "\\temp\\cache";
+        string data_path = Environment.CurrentDirectory + "\\data";
+
+        bool hastoken = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (!Directory.Exists(cache_path))
+                Directory.CreateDirectory(cache_path);
+
+            if (!Directory.Exists(data_path))
+                Directory.CreateDirectory(data_path);
+
+            MessageBox.Show(Int64.MaxValue.ToString());
+
+        }
+
+        public void setvisibility(System.Windows.Visibility state)
+        {
+            button_send.Visibility = state;
+            dialog_list.Visibility = state;
+            message_list.Visibility = state;
+            type_form.Visibility = state;
+            user_name.Visibility = state;
+            title.Visibility = state;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void button_close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void button_min_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //  FileStream user_data_file = File.OpenRead()
+            if (File.Exists(data_path + "\\u.vdf"))
+            {
+
+                FileStream file_read = File.OpenRead(data_path + "\\u.vdf");
+                BinaryReader data_reader = new BinaryReader(file_read);
+
+                userid = Convert.ToInt32(data_reader.ReadString());
+                token = data_reader.ReadString();
+
+                data_reader.Close();
+                file_read.Close();
+
+                data_reader = null;
+                file_read = null;
+
+                
+                if(Vk.CheckToken(userid,token))
+                {
+                    hastoken = true;
+                }
+                else
+                {
+                    hastoken = false;
+                }
+
+                /*try
+                {
+                   *File.Decrypt(data_path + "\\u.vdf");
+                    FileStream udf_file = File.OpenRead(data_path + "\\u.vdf");
+                    BinaryReader read_data = new BinaryReader(udf_file);
+                    userid = read_data.ReadInt32();
+                    token = read_data.ReadString();
+                    read_data.Close();
+                    udf_file.Close();
+                    File.Encrypt(data_path + "\\u.vdf");
+                    hastoken = true; 
+
+                    //File.Decrypt(data_path + "\\u.vdf");
+                    string[] lines;
+                    lines = File.ReadAllLines(data_path + "\\u.vdf");
+
+                    userid = Convert.ToInt32(lines[0]);
+                    token = lines[1];
+
+                    lines = null;
+
+                    //File.Encrypt(data_path + "\\u.vdf");
+
+                    Init_Interface();
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Ошибка авторизации! Код ошибки: 3", "SVKM Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    hastoken = false;
+                    if (File.Exists(data_path + "\\u.vdf"))
+                        File.Delete(data_path + "\\u.vdf");
+                }
+                catch (ObjectDisposedException)
+                {
+                    MessageBox.Show("Ошибка авторизации! Код ошибки: 4", "SVKM Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    hastoken = false;
+                    if (File.Exists(data_path + "\\u.vdf"))
+                        File.Delete(data_path + "\\u.vdf");
+                }
+
+                catch(FormatException)
+                {
+                    MessageBox.Show("Ошибка авторизации! Код ошибки: 5", "SVKM Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    hastoken = false;
+                    if (File.Exists(data_path + "\\u.vdf"))
+                        File.Delete(data_path + "\\u.vdf");
+                }
+
+                catch(IndexOutOfRangeException ex)
+                {
+                    MessageBox.Show("Ошибка авторизации! Код ошибки: 8.\n" + ex.Message.ToString(),"SVKM Error!",MessageBoxButton.OK, MessageBoxImage.Error);
+                    if (File.Exists(data_path + "\\u.vdf"))
+                        File.Delete(data_path + "\\u.vdf");
+                }*/
+
+            }
+
+            if (!hastoken)
+            {
+                vbrowser.Source = new Uri("https://oauth.vk.com/authorize?client_id=" + appid.ToString() + "&scope=audio,wall,messages,friends,groups&redirect_uri=https://oauth.vk.com/blank.html&display=popup&v=5.27&response_type=token");
+            }
+            else
+            {
+                Init_Interface(true);
+            }
+        }
+
+
+        public void Init_Interface(bool authorisation_ok)
+        {
+            vbrowser.Visibility = System.Windows.Visibility.Hidden;
+            setvisibility(System.Windows.Visibility.Visible);
+
+            if(authorisation_ok)
+            {
+                foreach(VkMessage n in Vk.GetDialogs(userid,token,200))
+                {
+                    ListBoxItem item = new ListBoxItem();
+                    
+                    if(n.is_chat)
+                    {
+                        item.Content = n.title;
+                        item.Uid = n.chat_id.ToString();
+                        item.Tag = true;
+                    }
+                    else
+                    {
+                        item.Content = n.user_id.ToString();
+                        item.Uid = n.user_id.ToString();
+                        item.Tag = false;
+                    }
+
+                    dialog_list.Items.Add(item);
+                    item = null; 
+
+                }
+            }
+        }
+
+
+        private void vbrowser_AddressChanged(object sender, Awesomium.Core.UrlEventArgs e)
+        {
+
+        }
+
+        private void vbrowser_DocumentReady(object sender, Awesomium.Core.UrlEventArgs e)
+        {
+            string url = vbrowser.Source.ToString();
+
+
+            if ((url.IndexOf("access_token") != -1) && (!hastoken))
+            {
+                Regex token_patt = new Regex("token=(.+)&expires_in");
+                Match token_match = token_patt.Match(url);
+                Regex user_patt = new Regex("user_id=(.+)");
+                Match user_match = user_patt.Match(url);
+
+                if ((!token_match.Success) || (!user_match.Success))
+                {
+                    MessageBox.Show("Ошибка!", "SVKM Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    token = token_match.Groups[1].Value.ToString();
+                    userid = Convert.ToInt32(user_match.Groups[1].Value.ToString());
+
+
+                    if (File.Exists(data_path + "\\u.vdf"))
+                    {
+                        File.Delete(data_path + "\\u.vdf");
+                    }
+
+
+                    //try
+                    //{
+                       /* BinaryWriter write_data = new BinaryWriter(File.OpenWrite(data_path + "\\u.vdf"));
+                        write_data.Write(userid);
+                        write_data.Write(token);
+
+                        write_data.Close();
+                        write_data = null;*/
+                        //File.Create(data_path + "\\u.vdf");
+                        //string[] lines = { userid.ToString(), token };
+                        //File.WriteAllLines(data_path + "\\u.vdf", lines);
+                        
+                      //  File.Encrypt(data_path + "\\u.vdf");
+                        
+                   // }
+                    /*catch (IOException ex)
+                    {
+                        MessageBox.Show("Ошибка сохранения данных! Код ошибки: 1."+"\n"+ex.Message.ToString(), "SVKM Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        if (File.Exists(data_path + "\\u.vdf"))
+                            File.Delete(data_path + "\\u.vdf");
+                    }
+                    catch (ObjectDisposedException ex)
+                    {
+                        MessageBox.Show("Ошибка сохранения данных! Код ошибки: 2."+"\n"+ex.Message.ToString(), "SVKM Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        if (File.Exists(data_path + "\\u.vdf"))
+                            File.Delete(data_path + "\\u.vdf");
+                    }
+                    
+                    catch(IndexOutOfRangeException ex)
+                    {
+                        MessageBox.Show("Ошибка сохранения данных! Код ошибки: 7.\n" + ex.Message.ToString(), "SVKM Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        if (File.Exists(data_path + "\\u.vdf"))
+                            File.Delete(data_path + "\\u.vdf");
+                    }
+
+                   /* try
+                    {
+                        File.Encrypt(data_path + "\\u.vdf");
+                    }
+                    catch(IOException ex)
+                    {
+                        MessageBox.Show("Ошибка сохранения данных! Код ошибки: 5." + "\n" + ex.Message.ToString(), "SVKM Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    } */
+
+                    FileStream file_writer = File.OpenWrite(data_path + "\\u.vdf");
+                    BinaryWriter data_writer = new BinaryWriter(file_writer);
+
+
+                    data_writer.Write(userid.ToString());
+                    data_writer.Write(token);
+                    data_writer.Close();
+
+                    file_writer.Close();
+
+                    data_writer = null;
+                    file_writer = null;
+
+                    Init_Interface(true);
+
+                }
+            }
+
+
+        }
+
+        private void dialog_list_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ListBoxItem it = (ListBoxItem)dialog_list.SelectedItem;
+            MessageBox.Show(it.Uid.ToString());
+            it = null;
+            MessageBox.Show("dfs");
+        }
+    }
+}
---
+++ new_svkm/vk.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Text;
+using System.Windows;
+
+namespace new_svkm
+{
+    struct VkAttachment
+    {
+       public string id;
+       public string source;
+       public string type;
+       public string owner_id;
+    }
+     class VkMessage
+     {
+         public String body;
+         public Int64 user_id;
+         public DateTime date;
+         public bool IsOut;
+         public bool is_chat;
+         public int chat_id;
+         public String title;
+         public bool is_read;
+
+         public List<VkAttachment> attachments;
+
+         public void FromXml(XmlNode n)
+         {
+  
+                    
+                 if (n.SelectSingleNode("out").InnerText == "0")
+                 {
+                     IsOut = false;
+                     user_id = Convert.ToInt64(n.SelectSingleNode("uid").InnerText);
+                 }
+                 else
+                 {
+                     IsOut = true;
+                 }
+
+                 if (n.SelectSingleNode("chat_id") != null)
+                 {
+               
+                     title = n.SelectSingleNode("title").InnerText;
+                     chat_id = Convert.ToInt32(n.SelectSingleNode("chat_id").InnerText);
+                     is_chat = true;
+                 }
+                 else
+                 {
+                     is_chat = false;
+                 }
+
+                 body = n.SelectSingleNode("body").InnerText;
+                 //date = Convert.ToDateTime(n.SelectSingleNode("date").InnerText);
+
+                 if (n.SelectSingleNode("read_state").InnerText == "0")
+                     is_read = true;
+                 else
+                     is_read = false;
+
+                if(n.SelectSingleNode("attachments") != null)
+                {
+                    
+                    foreach(XmlNode attach in n.SelectNodes("/attachments/attachment"))
+                    {
+                        VkAttachment attach_struct = new VkAttachment();
+                        string type = n.SelectSingleNode("type").InnerText;
+                      
+                        switch(type)
+                        {
+                            case "photo":
+                                {
+                                    attach_struct.type = type;
+                                    attach_struct.id = n.SelectSingleNode("/photo/pid").InnerText;
+                                    attach_struct.source = n.SelectSingleNode("/photo/src_big").InnerText;
+                                    attachments.Add(attach_struct);
+                                    break;
+                                }
+
+                            case "wall":
+                                {
+                                    attach_struct.type = type;
+                                    attach_struct.id = n.SelectSingleNode("/wall/id").InnerText;
+                                    attach_struct.owner_id = n.SelectSingleNode("/wall/owner_id").InnerText;
+                                    attachments.Add(attach_struct);
+
+                                    break;
+                                }
+
+
+                        }
+
+                    }
+                }
+
+                
+             
+         }
+
+         public void FromJson(string msg)
+         {
+
+         }
+
+     }
+    class Vk
+    {
+
+        private static XmlDocument VkAPI_Perform(string request, string token="", string par1="", string par2="",string par3="",string par4="",string par5="")
+        {
+            XmlDocument res = new XmlDocument();
+            
+            switch(request)
+            {
+                case "messages.getDialogs":
+                    {
+                        res.Load("https://api.vk.com/method/messages.getDialogs.xml?count=" + par1 + "&access_token=" + token);
+                        break;
+                    }
+
+                case "users.get":
+                    {
+                        res.Load("https://api.vk.com/method/users.get.xml?user_ids=" + par1 + "&fields=online&access_token=" + token);
+                        break;
+                    }
+            }
+
+            return res;
+        }
+
+        public static bool CheckToken(int userid, string token)
+        {
+            bool valid = false;
+
+            XmlDocument data = new XmlDocument();
+
+            data = VkAPI_Perform("users.get", token, userid.ToString());
+
+            if (data.SelectNodes("/response").Count != 0)
+            {
+                valid = true;
+            }
+            else
+                MessageBox.Show(data.InnerXml);
+
+            return valid;
+        }
+
+        public static List<VkMessage> GetDialogs(int userid, string token, int count)
+        {
+            List<VkMessage> res = new List<VkMessage>();
+            XmlDocument dialogs = new XmlDocument();
+            dialogs = VkAPI_Perform("messages.getDialogs", token, count.ToString());
+
+
+            if (dialogs.SelectNodes("/response").Count != 0)
+            {
+                foreach (XmlNode n in dialogs.SelectNodes("/response/message"))
+                {
+                    VkMessage msg = new VkMessage();
+                    msg.FromXml(n);
+                    res.Add(msg);
+                }
+            }
+
+            else 
+            {
+                MessageBox.Show("Ошибка! Полный текст:\n" + dialogs.InnerXml, "Svkm: error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+            MessageBox.Show(res.Count.ToString());
+            return res;
+        }
+    }
+}
